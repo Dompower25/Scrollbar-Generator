ac5bb677c3713c040f8ef0c4e2b9d44dd99c8849
Eugene Domanovich <dompro1994.ed@gmail.com>
2024-01-29 22:38:24 +0300

    no comment

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="wrapper">asd</div>
+</template>
+<style lang="scss">
+.wrapper {
+    display: block;
+    height: 100%;
+    background: black;
+}
+</style>
