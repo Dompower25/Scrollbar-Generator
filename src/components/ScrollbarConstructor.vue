497caa6b4643497b8d25c749579377f5d68aa603
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2024-03-03 16:22:14 +0000

    add style for scrollbar - in progres

--- src/components/ScrollbarConstructor.vue
+++ src/components/ScrollbarConstructor.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
 </script>
-<template></template>
-<style lang="scss"></style>
+    <template>
+        <section>
+            <div class="carollbar_box">
+                <div class="scrollbar"></div>
+            </div>
+        </section>
+    </template>
+<style lang="scss">
+    .carollbar_box {
+        width: 30px;
+        height: 322px;
+        background: #000000, 0.1%;
+        
+    }
+</style>
--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -2,7 +2,15 @@
 
 <template>
   <div class="wrapper">
-    <div class="scrollbar_style_block"></div>
+    <div class="scrollbar_style_block">
+      <section>
+            <div class="carollbar_box">
+                <div class="scrollbar">
+
+                </div>
+            </div>
+        </section>
+    </div>
     <div class="line"></div>
     <div class="css_style_block"></div>
   </div>
@@ -17,6 +25,30 @@
 
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
+
+  .carollbar_box {
+        width: 30px;
+        height: 322px;
+        background: rgba(255, 255, 255, 0.1);
+        display: flex;
+        justify-content: center;
+
+        padding: 15px 0px;
+
+
+
+        .scrollbar {
+          width: 19px;
+          height: 198px;
+          border-radius: 10px;
+          align-self: flex-end;
+          background: rgba(29, 35, 56, 1);
+        }
+        
+    }
+
 
   .line {
     width: 1px;
