d60f74d3fd8fcd9c42a0f02ab50845a5070575ba
Eugene Domanovich <dompro1994.ed@gmail.com>
2024-03-04 16:08:23 +0300

    *

--- src/components/ScrollbarConstructor.vue
+++ src/components/ScrollbarConstructor.vue
@@ -2,16 +2,8 @@
 </script>
     <template>
         <section>
-            <div class="carollbar_box">
-                <div class="scrollbar"></div>
-            </div>
+
         </section>
     </template>
 <style lang="scss">
-    .carollbar_box {
-        width: 30px;
-        height: 322px;
-        background: #000000, 0.1%;
-        
-    }
 </style>
--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -4,12 +4,10 @@
   <div class="wrapper">
     <div class="scrollbar_style_block">
       <section>
-            <div class="carollbar_box">
-                <div class="scrollbar">
-
-                </div>
-            </div>
-        </section>
+        <div class="scrollbar_box">
+          <div class="scrollbar"></div>
+        </div>
+      </section>
     </div>
     <div class="line"></div>
     <div class="css_style_block"></div>
@@ -23,31 +21,14 @@
   width: 70%;
   height: 500px;
 
+  border: 1px solid rgb(223, 120, 120);
+
   display: flex;
   justify-content: space-between;
   align-items: center;
+  box-sizing: border-box;
+  padding: 20px;
 
-
-  .carollbar_box {
-        width: 30px;
-        height: 322px;
-        background: rgba(255, 255, 255, 0.1);
-        display: flex;
-        justify-content: center;
-
-        padding: 15px 0px;
-
-
-
-        .scrollbar {
-          width: 19px;
-          height: 198px;
-          border-radius: 10px;
-          align-self: flex-end;
-          background: rgba(29, 35, 56, 1);
-        }
-        
-    }
 
 
   .line {
@@ -61,6 +42,34 @@
   .scrollbar_style_block {
     width: 100%;
     height: 100%;
+
+
+
+    section {
+      height: 100%;
+      display: flex;
+      
+      justify-content: space-around;
+      align-items: center;
+    }
+
+    .scrollbar_box {
+    width: 30px;
+    height: 322px;
+    background: rgba(255, 255, 255, 0.1);
+    display: flex;
+    justify-content: center;
+
+    padding: 15px 0px;
+
+    .scrollbar {
+      width: 19px;
+      height: 198px;
+      border-radius: 10px;
+      align-self: flex-end;
+      background: rgba(29, 35, 56, 1);
+    }
+  }
 
     border: 1px solid white;
   }
