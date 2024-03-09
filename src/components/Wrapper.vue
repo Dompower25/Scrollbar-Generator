4c929164aa068b37319c3dfc5c72878957d084f0
Eugene Domanovich <dompro1994.ed@gmail.com>
2024-03-09 15:25:46 +0300

    added svg for style scrollbar

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -5,7 +5,9 @@
     <div class="scrollbar_style_block">
       <section>
         <div class="scrollbar_box">
-          <div class="scrollbar"></div>
+          <div class="scrollbar">
+            <img src="../assets/Group 11.svg" alt="" srcset="" />
+          </div>
         </div>
       </section>
     </div>
@@ -29,8 +31,6 @@
   box-sizing: border-box;
   padding: 20px;
 
-
-
   .line {
     width: 1px;
     height: 100%;
@@ -43,33 +43,31 @@
     width: 100%;
     height: 100%;
 
-
-
     section {
       height: 100%;
       display: flex;
-      
+
       justify-content: space-around;
       align-items: center;
     }
 
     .scrollbar_box {
-    width: 30px;
-    height: 322px;
-    background: rgba(255, 255, 255, 0.1);
-    display: flex;
-    justify-content: center;
+      position: absolute;
 
-    padding: 15px 0px;
+      width: 30px;
+      height: 322px;
+      background: rgba(255, 255, 255, 0.1);
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-    .scrollbar {
-      width: 19px;
-      height: 198px;
-      border-radius: 10px;
-      align-self: flex-end;
-      background: rgba(29, 35, 56, 1);
+      padding: 15px 0px;
+
+      .scrollbar {
+        position: inherit;
+        transform: translateX(-2px);
+      }
     }
-  }
 
     border: 1px solid white;
   }
